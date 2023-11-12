74a889be800e0461b6e20bfe9aa658445b5c88a0
AndrewDoucet <92753789+AndrewDoucet@users.noreply.github.com>
2023-11-12 14:45:57 -0400

    Made responses an array of strings

--- main.go
+++ main.go
@@ -8,36 +8,25 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 	"github.com/sashabaranov/go-openai"
 )
 
-func homePage(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "Homepage!")
-	fmt.Println("Endpoint Hit: Homepage")
-}
-
-// Stores Person Information
-type person struct {
-	name  string
-	age   string
-	style string
-}
-
 // Stores Trip Information
 type trip struct {
-	city      string
-	days      string
-	budget    string
-	traveller person
+	city   string
+	days   string
+	budget string
+	age    string
 }
 
 var holiday trip
 var prompt string
 var plan string
-var plans [1]string
+var plans []string
 
 // Generates prompt based on trip parameters
 func makePrompt() string {
@@ -47,19 +36,27 @@ func makePrompt() string {
 	output += "Give me an itinerary for a trip to the city of " + holiday.city + ". "
 	output += "I am going for " + holiday.days + " days. "
 	output += "My budget is " + holiday.budget + " $. "
-	output += "I am " + holiday.traveller.age + ". "
-	output += "I am " + holiday.traveller.style + ". "
+	output += "I am " + holiday.age + ". "
+
+	/*for i := 0; i < len(holiday.traveller.wants); i++ {
+
+		if holiday.traveller.wants[i] == "true" {
+			output += "I want " + holiday.traveller.styles[i] + ". "
+		} else {
+			output += "I do not want " + holiday.traveller.styles[i] + ". "
+		}
+	}*/
+
 	output += "Name every establishment. "
 	output += "I only want to do one event every 3 hours, from 9AM to 9PM. "
 	output += "I want to see the most iconic things in this city. "
 	output += "Say how much every event will cost. "
-	output += "Say the word banana after"
 
 	return output
 }
 
 func makePlan() string {
-	client := openai.NewClient(os.Getenv("API_KEY"))
+	client := openai.NewClient("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")
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
@@ -81,18 +78,30 @@ func makePlan() string {
 	return resp.Choices[0].Message.Content
 }
 
+func homePage(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "Homepage!")
+	fmt.Println("Endpoint Hit: Homepage")
+}
+
 func submit(response http.ResponseWriter, request *http.Request) {
 
 	reqBody, _ := ioutil.ReadAll(request.Body)
 	json.Unmarshal(reqBody, &holiday)
 
+	holiday.city = "Halifax, Nova Scotia"
+	holiday.days = "2"
+	holiday.budget = "150"
+	holiday.age = "73"
+	// Generates Prompt
 	prompt = makePrompt()
 	fmt.Println(prompt)
 
+	// Makes Plan
 	plan = makePlan()
-	fmt.Println(plan)
 
-	plans[0] = plan
+	plans := strings.Split(plan, "\n")
+
+	// Output plan
 	json.NewEncoder(response).Encode(&plans)
 }
 
@@ -100,12 +109,6 @@ func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homePage)
 	router.HandleFunc("/i", submit)
-
-	bob := person{"Bob", "73", "Adventurous"}
-	holiday = trip{"London, Enlgand", "2", "35", bob}
-	prompt = makePrompt()
-	prompt = "Give me a word"
-	fmt.Println(prompt)
 
 	c := cors.New(cors.Options{
 		AllowCredentials: true,
