f4883038ff6c4cf8a7c3fde5db7c2b3c4ee77209
AndrewDoucet <92753789+AndrewDoucet@users.noreply.github.com>
2023-11-12 12:29:37 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -13,6 +15,55 @@ import (
 func homePage(response http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(response, "Homepage!")
 	fmt.Println("Endpoint Hit: Homepage")
+}
+
+// Stores Person Information
+type person struct {
+	name  string
+	age   string
+	style string
+}
+
+// Stores Trip Information
+type trip struct {
+	city      string
+	days      string
+	budget    string
+	traveller person
+}
+
+// Generates prompt based on trip parameters
+func (this trip) prompt() string {
+
+	var prompt string = ""
+
+	prompt += "Give me an itinerary for " + this.city + ". "
+	prompt += "I am going for " + this.days + " days. "
+	prompt += "My budget is " + this.budget + " $. "
+	prompt += "I am " + this.traveller.age + ". "
+	prompt += "I am " + this.traveller.style + ". "
+	prompt += "Name every establishment. "
+	prompt += "I only want to do one event every 3 hours, from 9AM to 9PM. "
+	prompt += "I want to see the most iconic things in this city. "
+	prompt += "// Say how much every event will cost."
+
+	return prompt
+}
+
+func (this trip) plan() string {
+
+	var plan string = this.prompt()
+
+	// GPT request
+
+	return plan
+}
+
+func submission(response http.ResponseWriter, request *http.Request) {
+	var newTrip trip
+	reqBody, _ := ioutil.ReadAll(request.Body)
+	json.Unmarshal(reqBody, &newTrip)
+	json.NewEncoder(response).Encode(newTrip)
 }
 
 func main() {
@@ -31,5 +82,11 @@ func main() {
 	}
 
 	log.Fatal(http.ListenAndServe(":"+port, handler))
+
+	/*
+		bob := person{"Bob", "73", "Adventurous"}
+		holiday := trip{"Halifax", "2", "35", bob}
+		fmt.Println(holiday.prompt())
+	*/
 
 }
